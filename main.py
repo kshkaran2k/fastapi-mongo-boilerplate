b2d4843f15a40a3dd9969aa9c495d9c47e8f71e5
kaushik karan <kshkaran2k@gmail.com>
2026-01-11 17:28:20 +0530

    updated app title

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
+
 from app.db.mongodb import MongoDB
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    # On Startup
     await MongoDB.init()
     yield
-    # On Shutdown
     await MongoDB.close()
 
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(title="FastAPI Mongo Boilerplate", lifespan=lifespan)
 
 
 @app.get("/")
